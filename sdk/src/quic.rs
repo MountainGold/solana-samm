490d6e6c63b707485453aa0f1606d51e599ac09c
Hongyin Chen <1600012903@pku.edu.cn>
2024-07-05 16:23:31 +0300

    Update quic.rs

--- sdk/src/quic.rs
+++ sdk/src/quic.rs
@@ -33,15 +33,18 @@ pub const QUIC_CONNECTION_HANDSHAKE_TIMEOUT: Duration = Duration::from_secs(60);
 
 /// The receive window for QUIC connection from unstaked nodes is
 /// set to this ratio times [`solana_sdk::packet::PACKET_DATA_SIZE`]
-pub const QUIC_UNSTAKED_RECEIVE_WINDOW_RATIO: u64 = 128;
+pub const QUIC_UNSTAKED_RECEIVE_WINDOW_RATIO: u64 = 1280;
+// pub const QUIC_UNSTAKED_RECEIVE_WINDOW_RATIO: u64 = 128;
 
 /// The receive window for QUIC connection from minimum staked nodes is
 /// set to this ratio times [`solana_sdk::packet::PACKET_DATA_SIZE`]
-pub const QUIC_MIN_STAKED_RECEIVE_WINDOW_RATIO: u64 = 128;
+pub const QUIC_MIN_STAKED_RECEIVE_WINDOW_RATIO: u64 = 1280;
+// pub const QUIC_MIN_STAKED_RECEIVE_WINDOW_RATIO: u64 = 128;
 
 /// The receive window for QUIC connection from maximum staked nodes is
 /// set to this ratio times [`solana_sdk::packet::PACKET_DATA_SIZE`]
-pub const QUIC_MAX_STAKED_RECEIVE_WINDOW_RATIO: u64 = 512;
+// pub const QUIC_MAX_STAKED_RECEIVE_WINDOW_RATIO: u64 = 512;
+pub const QUIC_MAX_STAKED_RECEIVE_WINDOW_RATIO: u64 = 5120;
 
 pub trait NotifyKeyUpdate {
     fn update_key(&self, key: &Keypair) -> Result<(), Box<dyn std::error::Error>>;
