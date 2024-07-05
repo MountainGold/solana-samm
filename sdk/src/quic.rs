8f0d60ee1112b3569854e52bc6e36f350bb3c150
Hongyin Chen <1600012903@pku.edu.cn>
2024-07-05 12:34:36 +0300

    Update quic.rs

--- sdk/src/quic.rs
+++ sdk/src/quic.rs
@@ -2,20 +2,26 @@
 //! Definitions related to Solana over QUIC.
 use {crate::signer::keypair::Keypair, std::time::Duration};
 
-pub const QUIC_PORT_OFFSET: u16 = 6;
+pub const QUIC_PORT_OFFSET: u16 = 60;
+// pub const QUIC_PORT_OFFSET: u16 = 6;
 // Empirically found max number of concurrent streams
 // that seems to maximize TPS on GCE (higher values don't seem to
 // give significant improvement or seem to impact stability)
-pub const QUIC_MAX_UNSTAKED_CONCURRENT_STREAMS: usize = 128;
-pub const QUIC_MIN_STAKED_CONCURRENT_STREAMS: usize = 128;
+// pub const QUIC_MAX_UNSTAKED_CONCURRENT_STREAMS: usize = 128;
+// pub const QUIC_MIN_STAKED_CONCURRENT_STREAMS: usize = 128;
 
-pub const QUIC_TOTAL_STAKED_CONCURRENT_STREAMS: usize = 100_000;
+pub const QUIC_MAX_UNSTAKED_CONCURRENT_STREAMS: usize = 1280;
+pub const QUIC_MIN_STAKED_CONCURRENT_STREAMS: usize = 1280;
+
+// pub const QUIC_TOTAL_STAKED_CONCURRENT_STREAMS: usize = 100_000;
+pub const QUIC_TOTAL_STAKED_CONCURRENT_STREAMS: usize = 1000000;
 
 // Set the maximum concurrent stream numbers to avoid excessive streams.
 // The value was lowered from 2048 to reduce contention of the limited
 // receive_window among the streams which is observed in CI bench-tests with
 // forwarded packets from staked nodes.
-pub const QUIC_MAX_STAKED_CONCURRENT_STREAMS: usize = 512;
+// pub const QUIC_MAX_STAKED_CONCURRENT_STREAMS: usize = 512;
+pub const QUIC_MAX_STAKED_CONCURRENT_STREAMS: usize = 5120;
 
 pub const QUIC_MAX_TIMEOUT: Duration = Duration::from_secs(2);
 pub const QUIC_KEEP_ALIVE: Duration = Duration::from_secs(1);
